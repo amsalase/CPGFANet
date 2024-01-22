d1f32e573c69294149da6698e7530806cc2e3f79
Andres Salas-Espinales <andres_salas_esp@hotmail.com>
2024-01-22 18:41:24 +0000

    con lr=0.03 y 0.5 en dice y softcrossentropy

--- train_dual_sinINF_2.py
+++ train_dual_sinINF_2.py
@@ -63,7 +63,7 @@ def train(epo, model, train_loader, optimizer):
         ##FocalLoss_fn = FocalLoss(mode="multiclass")
         SoftCrossEntropy_fn = SoftCrossEntropyLoss(smooth_factor=0.1)
         ##criterion = L.JointLoss(first=FocalLoss_fn, second=SoftCrossEntropy_fn,first_weight=0.5, second_weight=0.5).cuda()
-        criterion = L.JointLoss(first=DiceLoss_fn, second=SoftCrossEntropy_fn,first_weight=0.8, second_weight=0.8).cuda()
+        criterion = L.JointLoss(first=DiceLoss_fn, second=SoftCrossEntropy_fn,first_weight=0.5, second_weight=0.5).cuda()
         ##criterion = nn.CrossEntropyLoss()
         ##criterion = CriterionOhemDSN(thresh=0.6, min_kept=200000) ## apply loss of CCNet
         ##FolalLoss_fn = FocalLoss(mode="multiclass")
@@ -200,16 +200,16 @@ if __name__ == '__main__':
     optimizer = torch.optim.SGD(model.parameters(), lr=args.lr_start, momentum=0.9, weight_decay=0.0005)
     scheduler = torch.optim.lr_scheduler.ExponentialLR(optimizer, gamma=args.lr_decay, last_epoch=-1)
 
-    weight_dir = os.path.join("./runs_dual_2.2.dice0.8_ce0.8/", args.model_name)
+    weight_dir = os.path.join("./runs_dual_2.2_lr0.03v2.0/", args.model_name)
     if not os.path.exists(weight_dir):
         os.makedirs(weight_dir)
     os.chmod(weight_dir,
              stat.S_IRWXO)  # allow the folder created by docker read, written, and execuated by local machine
 
-    writer = SummaryWriter("./runs_dual_2.2.dice0.8_ce0.8/tensorboard_log")
-    os.chmod("./runs_dual_2.2.dice0.8_ce0.8/tensorboard_log",
+    writer = SummaryWriter("./runs_dual_2.2_lr0.03v2.0/tensorboard_log")
+    os.chmod("./runs_dual_2.2_lr0.03v2.0/tensorboard_log",
              stat.S_IRWXO)  # allow the folder created by docker read, written, and execuated by local machine
-    os.chmod("./runs_dual_2.2.dice0.8_ce0.8", stat.S_IRWXO)
+    os.chmod("./runs_dual_2.2_lr0.03v2.0", stat.S_IRWXO)
 
     print('training %s on GPU #%d with pytorch' % (args.model_name, args.gpu))
     print('from epoch %d / %s' % (args.epoch_from, args.epoch_max))
